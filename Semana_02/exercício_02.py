bd63d71fefad3a10156b60b76dd83bf661fb7859
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-08-22 15:15:02 -0300

    Update

--- Semana_02/exercício_02.py
+++ Semana_02/exercício_02.py
@@ -2,15 +2,20 @@ def remove_duplicates(list_1):
     new_list_clean = []
     duplicates = []
     for i in range(len(list_1)):
-        if list_1[i] != list_1:
+        if list_1[i] not in new_list_clean:
             new_list_clean.append(list_1[i])        
         else: 
             duplicates.append(list_1[i])
+    return new_list_clean, duplicates
 
 def main():
-    list_1 = [1, 2, 3, 4, 4, 5, 2]
-    new_list_clean = remove_duplicates(list_1)
-    duplicates = remove_duplicates(list_1)
+    list_1 = []
+    while True:
+        list_1.append(int(input("Digite um número: ")))
+        r = input("Quer continuar? S/N   ").upper()
+        if r == "N":
+            break
+    new_list_clean, duplicates = remove_duplicates(list_1)
     print("Lista limpa: ", new_list_clean)
     print("Duplicadas: ", duplicates)
 main()
