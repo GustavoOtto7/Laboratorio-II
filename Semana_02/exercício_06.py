6334e777f54cafcd7955ae632633aa8e9867384b
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-08-22 22:14:57 -0300

    update

--- Semana_02/exercício_06.py
+++ Semana_02/exercício_06.py
@@ -2,14 +2,15 @@ def check_simmetric(matriz):
     n = len(matriz)
     for i in range(n):
             for j in range(n):
-                if matriz[i][j] != matriz[j][i] and matriz[j][i] != matriz[i][j]:
+                if matriz[i][j] != matriz[j][i]:
                     print("A sua matriz quadrada não é simétrica!")
-                    break
     print("A sua matriz quadrada é simétrica!")
+
 def main():
     matriz = [
-        [1, 2],
-        [2, 2]
+        [1, 2, 8],
+        [2, 4, 5],
+        [3, 5, 6]
     ]
     check_simmetric(matriz)
 main()
---
+++ Semana_02/exercício_07.py
@@ -0,0 +1,7 @@
+
+
+def main():
+    original_numbers 
+
+
+main()
