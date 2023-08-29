066c377e46847e1edb563254a5c0e84df76d4b65
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-08-29 19:47:30 -0300

    update

---
+++ Semana_03/exercicio_01.py
@@ -0,0 +1,26 @@
+import random
+
+def validate_number(matriz, num):
+    for line in range(len(matriz)):
+        for column in range(len(matriz[line])):
+            if matriz[line][column] == num:
+                return False
+    return True
+
+def generate_bingo():
+    matriz = []
+    for line in range(0,5):
+        matriz.append([])
+        for column in range(0, 5):
+            num = random.randint(0, 99)
+            while not validate_number(matriz, num):
+                num = random.randint(0, 99)
+            matriz[line].append(num)   
+    return matriz
+
+def main():
+    matriz = generate_bingo()
+    print(matriz)    
+
+
+main()
