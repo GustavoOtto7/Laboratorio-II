84fb95e6df61e3207ea2badc357c9b8443537fae
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-09-04 17:24:40 -0300

    up

--- Semana_02/exercício_07.py
+++ Semana_02/exercício_07.py
@@ -1,7 +1,15 @@
-
+def verify(original_numbers):
+    new_list = []
+    for i in range(0, 12):
+        if original_numbers[i] - 1 not in original_numbers:
+            new_list.append(f"[{original_numbers[i]} - ")
+        elif original_numbers[i] + 1 not in original_numbers:
+            new_list.append(f"{original_numbers[i]}]")
+    return new_list
+        
 
 def main():
-    original_numbers 
-
-
+    original_numbers = [100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150]
+    new_list = verify(original_numbers)
+    print(new_list)
 main()
