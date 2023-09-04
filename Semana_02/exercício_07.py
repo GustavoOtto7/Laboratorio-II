cadf16fb35affcbadef34dcc98d5b5205faffc16
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-09-04 17:25:54 -0300

    up

--- Semana_02/exercício_07.py
+++ Semana_02/exercício_07.py
@@ -5,8 +5,7 @@ def verify(original_numbers):
             new_list.append(f"[{original_numbers[i]} - ")
         elif original_numbers[i] + 1 not in original_numbers:
             new_list.append(f"{original_numbers[i]}]")
-    return new_list
-        
+    return new_list    
 
 def main():
     original_numbers = [100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150]
