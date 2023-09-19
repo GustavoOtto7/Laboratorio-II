6335a61037f4a1a8b01a55ae76b8209c00f3c0a9
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-09-19 14:06:07 -0300

    up

--- Semana_06 - (Trabalho)/estoque.py
+++ Semana_06 - (Trabalho)/estoque.py
@@ -1,5 +1,5 @@
 def add_product(stock):
-    """Verifica e adiciona um produto no estoque."""
+    """Adiconar/Verificar um Produto ao Estoque"""
     product_name = input("Digite o nome do produto: ")
     if product_name == '':
         return
@@ -9,11 +9,12 @@ def add_product(stock):
     else:
         product_amount = int(input("Digite a quantidade do produto: "))
         product_price = float(input("Digite o preço do produto: R$"))
-        product_category = input("Digite a categoria do produto: ")
+        product_category = input("Digite uma Categoria: ")
         stock[product_name] = {
             "amount": product_amount,
             "price": product_price,
             "category": product_category}
+        
     for product_name, product_info in stock.items():
         amount = product_info["amount"]
         price = product_info["price"]
@@ -22,21 +23,60 @@ def add_product(stock):
     print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}, Categoria: {category}")
     return stock
 
-def find_product(stock):
-    """Fazer a busca no estoque por um produto específico."""
-    product_name = input("Digite o nome do Produto: ")
+def change_product_value(stock):
+    """Alterar Valor de Produto"""
+    product_name = input("Qual produto deseja mudar o valor ? ")
     if product_name in stock:
-        print(f"Procurando {product_name}...")
-        for amount, price in stock[product_name].items():
+        new_price = float(input(f"Digite novo valor para {product_name}: "))
+        stock[product_name]["price"] = new_price
+    return stock
+    
+def find_product_category(stock):
+    """Buscar produto no estoque / Categoria no Stock"""
+    print("1 - Buscar por nome do Produto")
+    print("2 - Buscar por categoria")
+    print("3 - Sair")
+    opc = int(input("Escolha uma opção: "))
+    if opc == 1:
+        product_name = input("Digite o nome do Produto: ")
+        if product_name in stock:
+            print(f"Procurando {product_name}...")
+            
             amount = stock[product_name]["amount"]
             price = stock[product_name]["price"]
-        print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}")
-    else:
-        print(f"Produto {product_name} não registrado")
+            category = stock[product_name]["category"]
 
+            print(f"Produto: {product_name}")
+            print(f"Quantidade: {amount}") 
+            print(f"Preço: {price}")
+            print(f"Categoria: {category}")
+        else:
+            print(f"Produto {product_name} não registrado")
+    elif opc == 2:
+        product_category = input("Digite a categoria: ")
+
+        for product in stock.values():
+            if product['category'] == product_category:
+                print(product)
+                   
+        else:
+            print(f"Categoria não encontrada.")
+            return
+    elif opc == 3:
+        return
+
+
+def remover_product(stock):
+    """Remover Produto"""
+    product_name = input("Digite o produto que deseja remover: ")
+    if product_name in stock:
+        stock.pop(product_name)
+    return stock
+        
 def show_stock(stock):
-    """Mostra todos os produtos do estoque."""
+    """Mostrar completamente o estoque"""
     for product_name, product_info in stock.items():
         amount = product_info["amount"]
         price = product_info["price"]
-        print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}")
+        category = product_info["category"]
+        print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}, Categoria: {category}")
--- Semana_06 - (Trabalho)/main.py
+++ Semana_06 - (Trabalho)/main.py
@@ -1,12 +1,12 @@
-from estoque import add_product, find_product, show_stock
+from estoque import add_product, find_product_category, show_stock, change_product_value, remover_product
 from vendas import sale_product, sales_report
 from menuzao import show_menu
-
+        
 def main():
     stock = {}
     sales_dict = {}
-    opc = 0
-    while opc <= 5:
+    vendas = []
+    while True:
         opc = show_menu()
         if opc == 1:
             while True:
@@ -14,15 +14,18 @@ def main():
                 if input("Deseja adicionar outro produto? (s/n): ").lower() != "s":
                     break
         elif opc == 2:
-            find_product(stock)
+            stock = change_product_value(stock)
         elif opc == 3:
-            show_stock(stock)
+            stock = remover_product(stock)
         elif opc == 4:
+            find_product_category(stock)
+        elif opc == 5:
+            show_stock(stock)
+        elif opc == 6:
             while True:
                 stock = sale_product(stock, sales_dict)
                 if input("Deseja vender outro produto? (s/n): ").lower() != "s":
                     break
-        elif opc == 5:
+        elif opc == 7:
             sales_report(sales_dict)
-    print("Você saiu!")
 main()
