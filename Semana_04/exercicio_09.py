93b038ee18f8cfccb2710288f1c0320ba308c10d
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-09-19 23:22:25 -0300

    up

--- Semana_04/exercicio_09.py
+++ Semana_04/exercicio_09.py
@@ -1,15 +1,17 @@
 def result(students_dict):
-    name = str(input("Digite o nome do aluno: "))
+    name = input("Digite o nome do aluno: ")
     if name == "oooo":
-        for name, media in students_dict.items():
-            if students_dict[0]:
-                highest_name = name(students_dict[media])
-                highest_average = media(students_dict[name])
-            else:
-                if highest_average > media(students_dict[name]):
-                    highest_average = media(students_dict[name])
-                    highest_name = name(students_dict[media])
+        highest_name = None
+        highest_average = 0
+        for name, data in students_dict.items():
+            media = data["dados"]
+            if media > highest_average:
+                highest_average = media
+                highest_name = name
+        if highest_name:
             print(f"A maior nota média foi do aluno {highest_name} com {highest_average} pts")
+        else:
+            print("Nenhum aluno encontrado.")
         return False
     else:
         g1 = float(input("Digite a primeira nota do aluno: "))
