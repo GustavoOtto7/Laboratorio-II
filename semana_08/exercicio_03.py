01e89bcb51ae1959c78cedba493fcd010c4de4d5
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-10-17 21:29:05 -0300

    up

--- semana_08/exercicio_03.py
+++ semana_08/exercicio_03.py
@@ -1,7 +1,7 @@
-def division(month): 
-    divi_by_4 = month % 4
-    divi_by_100 = month % 100
-    divi_by_400 = month % 400
+def division(year): 
+    divi_by_4 = year % 4
+    divi_by_100 = year % 100
+    divi_by_400 = year % 400
     if divi_by_400 == 0: 
         result = str("Bissexto")
     elif divi_by_4 ==  0 and divi_by_100 != 0:
@@ -9,11 +9,17 @@ def division(month):
     else: 
         result = str("Não Bissexto!")
     return result
+'''
+def is_leap_year(year):
+    if (year % 4 == 0 and not year % 100 = 0) or year % 400 == 0:
+        return True
+    return False
+'''
 
 def main():
     try:
-        month = int(input("Digite o ano desejado: "))
-        result = division(month)
+        year = int(input("Digite o ano desejado: "))
+        result = division(year)
         print("O ano que você digitou é: ", result)
     except ValueError:
         print("[Erro] Você digitou um valor inválido!")
