1ae45fbea793ad7c41d0b86c48e43c5429e00d88
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2023-11-21 10:56:09 -0300

    up

--- Semana_11 - (Termo)/play_game.py
+++ Semana_11 - (Termo)/play_game.py
@@ -1,33 +1,32 @@
 import random
 
 def get_word():
-    words_list = open("Laboratorio-II\Semana_11 - (Termo)\words_list.txt", 'r')
-    lines = words_list.readlines()    
-    words_list.close()
-    num_ind = random.randint(0, 8)
+    with open("Laboratorio-II\\Semana_11 - (Termo)\\words_list.txt", "r") as words_list:
+        lines = words_list.readlines()    
+    num_ind = random.randint(0, 8) #depois colocar o tamanho do Len
     num_lines = len(lines) - 1
     print("Número aleatório sorteado: ",num_ind)
     if num_ind <= num_lines:
-        word_sorted = lines[num_ind]
+        draw_word = lines[num_ind]
         print(lines[num_ind])
-        return word_sorted
+        return draw_word
     else:
         print('Número muito grande!')
 
-def check_word(word_sorted):
-    used_words_list = open("C:\Users\gusta\Repositórios VS Code\Laboratório-II\Laboratorio-II\Semana_11 - (Termo)\used_words_list.txt", "r")
-    list_used = used_words_list.readlines()
-    used_words_list.close()
-    for word in list_used():
-        if word == word_sorted:
+def check_word(draw_word):
+    with open("Laboratorio-II\\Semana_11 - (Termo)\\used_words_list.txt", "r") as used_words_list:
+        list_used = used_words_list.readlines()
+    for word in list_used:
+        if draw_word != word:
             print("Essa palavra já foi utilizada!")
             get_word()
-            break
+            break            
         else:
-            return word_sorted
+            return draw_word
 
-
-def playing_game():
+def playing_game(draw_word):
+    with open("Laboratorio-II\\Semana_11 - (Termo)\\used_words_list.txt", "a") as used_words_list:
+        used_words_list.write(draw_word)
     for i in range(0, 5):
         word = (list([input("Digite a palavra desejada: ").upper()]))
         for letter in word:
@@ -35,9 +34,9 @@ def playing_game():
 
 
 def main():
-    word_sorted = get_word()
-    check_word(word_sorted)
-    playing_game()
+    draw_word = get_word()
+    check_word(draw_word)
+    playing_game(draw_word)
 
 
 main()
